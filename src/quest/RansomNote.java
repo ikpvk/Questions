3c999bed8305a98332a44bd90a6667bc5aaf272a
Krishna Prasad <krishnavk@kloudgin.com>
2021-02-27 21:31:41 +0530

    Adding ransom note solution

---
+++ src/quest/RansomNote.java
@@ -0,0 +1,30 @@
+package quest;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        String ransomNote = "abba";
+        String magazine = "aabb";
+        System.out.println(canConstruct(ransomNote,magazine));
+    }
+
+    public static boolean canConstruct( String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int letters[] = new int[26];
+
+        for(char magazineLetter : magazine.toCharArray()) {
+            letters[magazineLetter-'a']++;
+        }
+
+        for(char ransomNoteLetter : ransomNote.toCharArray()) {
+            if(letters[ransomNoteLetter-'a']==0) {
+                return false;
+            } else {
+                letters[ransomNoteLetter-'a']--;
+            }
+        }
+        return true;
+    }
+}
