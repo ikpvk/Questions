07b057a536f8b1c1117afdf3b6ad656c3a777d1e
krishna <krishnaprasad.vk@gmail.com>
2021-08-23 01:16:29 +0530

    Adding new questions || Largest Palindrome substring

--- src/quest/longestpalindromesubstring/App.java
+++ src/quest/longestpalindromesubstring/App.java
@@ -2,6 +2,21 @@ package quest.longestpalindromesubstring;
 
 public class App {
     public static void main(String[] args) {
+        String value = "cabba";
+        int start=0, end = 0;
+
+        Solution solution = new Solution();
+        for(int i=0; i< value.length()-1; i++) {
+            int len1 = solution.getMaxPalindromeLength(value,i,i);
+            int len2 = solution.getMaxPalindromeLength(value,i,i+1);
+            int maxLen = Math.max(len1,len2);
+            if(maxLen>(end-start-1)) {
+                start = i - ((maxLen-1)/2);
+                end = i + (maxLen/2);
+            }
+        }
+
+        System.out.println("Longest substring of "+value+" is : "+value.substring(start, end+1));
 
     }
 }
--- src/quest/longestpalindromesubstring/Solution.java
+++ src/quest/longestpalindromesubstring/Solution.java
@@ -1,4 +1,17 @@
 package quest.longestpalindromesubstring;
 
 public class Solution {
+    public int getMaxPalindromeLength(String value, int left, int right) {
+
+        if(null==value || value.trim().length()<1 || left < 0 || right>=value.length()) {
+            return 0;
+        }
+
+        while(left>=0 && right<value.length() && value.charAt(left)==value.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right-left-1;
+    }
 }
