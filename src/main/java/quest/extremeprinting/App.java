3f70b4f808b2e6a5f6a9fb79baa8d530611f80fa
Krishna <krishnaprasad.vk@gmail.com>
2024-08-18 15:49:35 +0530

    Adding code for extreme printing of arrays

---
+++ src/main/java/quest/extremeprinting/App.java
@@ -0,0 +1,10 @@
+package quest.extremeprinting;
+
+public class App {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,100,6,7,8,9,20};
+        Solution sol = new Solution();
+        sol.printExtremes(arr);
+
+    }
+}
---
+++ src/main/java/quest/extremeprinting/Solution.java
@@ -0,0 +1,20 @@
+package quest.extremeprinting;
+
+public class Solution {
+    public void printExtremes(int[] arr){
+        if(null==arr || arr.length==0) {
+            return;
+        }
+
+        int length = arr.length;
+        int leftP = 0, rightP = length-1;
+        while(leftP<rightP) {
+            System.out.print(arr[leftP]+ " " + arr[rightP]+ " ");
+            leftP++;
+            rightP--;
+        }
+        if(leftP==rightP) {
+            System.out.print(arr[leftP]);
+        }
+    }
+}
